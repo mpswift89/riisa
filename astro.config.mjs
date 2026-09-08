9d8931686b8f2d6fa5617a0bbd3be867d33270dd
Miguel Mercado <mpswift89@gmail.com>
2026-09-08 16:38:41 -0400

    Fix deployment build script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://mpswift89.github.io',
+  base: '/riisa',
   vite: {
     plugins: [tailwindcss()],
   },
